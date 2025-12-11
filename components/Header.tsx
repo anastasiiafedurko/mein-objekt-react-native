8f0fdbdf53fe67cf9c6686c1b3d316799b153b52
FernirTeam <anastasiiafedurko@gmail.com>
2025-12-11 13:42:01 +0100

    create welcomescreen

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  Image,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Header as HeaderRNE } from "@rneui/themed";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+interface HeaderProps {
+  museumName: string;
+}
+
+export const Header: React.FunctionComponent<HeaderProps> = ({
+  museumName,
+}) => {
+  return (
+    <SafeAreaProvider>
+      <HeaderRNE
+        leftComponent={
+          <View style={styles.headerRight}>
+            <Image
+              source={require("../assets/images/museum-logo.png")}
+              style={styles.image}
+            />
+          </View>
+        }
+        rightComponent={{
+          icon: "settings",
+          color: "#fff",
+        }}
+        centerComponent={{ text: museumName, style: styles.heading }}
+      />
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#397af8",
+    marginBottom: 20,
+    width: "100%",
+    paddingVertical: 15,
+  },
+  heading: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  headerRight: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  image: {
+    width: 20,
+    height: 20,
+  },
+});
+
+export default Header;
--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,5 +1,27 @@
-export default function Welcome() { 
-  return (
-    
-  )
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "@rneui/themed";
+
+interface WelcomeScreenProps {
+  museumName: string;
 }
+
+export const WelcomeScreen: React.FunctionComponent<WelcomeScreenProps> = ({
+  museumName,
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to {museumName}</Text>
+      <Button title="Start" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    gap: 10,
+  },
+});
+
+export default WelcomeScreen;
